db0e9a749bca336f713053f23039abcdb8c99586
MyeongSeong <ms1994s@naver.com>
2022-11-10 09:00:01 +0900

    Unit1 - ComposableArticle

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -24,25 +25,46 @@ import com.example.composearticle.ui.theme.ComposeArticleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeArticleTheme{
+                Surface(color = Color.White) {
+                    ComposeArticleApp()
+                }
+            }
+
+        }
     }
 }
 
 @Composable
-fun ComposeArticleApp() { }
+fun ComposeArticleApp() { ArticleCard(stringResource(id = R.string.title_jetpack_compose_tutorial), stringResource(id = R.string.compose_short_desc), stringResource(id = R.string.compose_long_desc), painterResource(id = R.drawable.bg_compose_background), modifier = Modifier.background(Color.White) ) }
 
 @Composable
-private fun ArticleCard(
-    title: String,
-    shortDescription: String,
-    longDescription: String,
-    imagePainter: Painter,
-    modifier: Modifier = Modifier,
-) {
-    Column() { }
+private fun ArticleCard(title: String, shortDescription: String, longDescription: String, imagePainter: Painter, modifier: Modifier = Modifier, ) {
+    Column() {
+        // 이미지 영역
+        Image(painter = imagePainter, contentDescription = null)
+        // 타이틀 영역
+        Text(text = title, fontSize = 24.sp , modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.Start)
+            .padding(all = 16.dp))
+        // 중간 영역
+        Text(text = shortDescription ,modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .padding(start = 16.dp, end = 16.dp), textAlign = TextAlign.Justify)
+        Text(text = longDescription ,modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .wrapContentHeight(Alignment.Bottom)
+            .padding(all = 16.dp), textAlign = TextAlign.Justify)
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ArticleCard(stringResource(id = R.string.title_jetpack_compose_tutorial), stringResource(id = R.string.compose_short_desc), stringResource(id = R.string.compose_long_desc), painterResource(id = R.drawable.bg_compose_background), Modifier )
+}
